5681e889c0cdbdf551185eb5b5ba2ac36efb88aa
moldham713 <moldham713@gmail.com>
2019-10-25 00:22:18 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -20,22 +20,55 @@
  * A9: A
  * A10: B
  * A11: C
+ *
+ * LED MATRIX PINOUT
+ * R1	G1
+ * B1	GND
+ * R2	G2
+ * B2	GND
+ * A 	B
+ * C 	GND
+ * CLK  LAT
+ * OE   GND
  */
 
 #include "stm32f0xx.h"
 #include "stm32f0_discovery.h"
 
-unsigned char * imagedata = malloc(32*3*16);
-unsigned int imageline = 0;
+unsigned char  imagedata[32*3*16] = {0};
 
 
-unsigned char init_picture()
+
+void init_picture()
 {
 	for(int x = 0; x < 32*3; x++)
 	{
 		for(int y = 0; y < 16; y++)
 		{
 			imagedata[(32*3*y)+x] = 1;
+		}
+	}
+}
+
+void picture_blank()
+{
+	for(int x = 0; x < 32*3; x++)
+	{
+		for(int y = 0; y < 16; y++)
+		{
+			imagedata[(32*3*y)+x] = 0;
+		}
+	}
+}
+
+void picture_red()
+{
+	for(int x = 0; x < 32*3; x++)
+	{
+		for(int y = 0; y < 16; y++)
+		{
+			if(x % 3 == 0) imagedata[(32*3*y)+x] = 1;
+			else imagedata[(32*3*y)+x] = 0;
 		}
 	}
 }
@@ -49,30 +82,25 @@ void init_GPIO()
 void init_tim6()
 {
 	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
-    TIM6->PSC = 16000-1;
-    TIM6->ARR = 100-1;
+    TIM6->PSC = 10-1;
+    TIM6->ARR = 10-1;
     TIM6->CR1 |= 1;
     TIM6->DIER |= 1;
     NVIC->ISER[0] |= 1 << (TIM6_DAC_IRQn);
 }
 
-void set_row()
-{
-	GPIOA->ODR &= !(0xe00); //Clear bits 9-11 for ABC
-	GPIOA->ODR |= (imageline & 1) << 9;
-	GPIOA->ODR |= (imageline & 2) << 10;
-	GPIOA->ODR |= (imageline & 3) << 11;
-}
-
 
 void TIM6_DAC_IRQHandler()
 {
-    TIM6->SR &= !(1);
+	static int imageline = 0;
+	GPIOA->ODR &= ~(0xe00); //Clear bits 9-11 for ABC
     GPIOA->ODR |= 1; //OEN
-    set_row();
+	GPIOA->ODR |= (imageline & 1) << 9;
+	GPIOA->ODR |= ((imageline & 2) >> 1) << 10;
+	GPIOA->ODR |= ((imageline & 4) >> 2) << 11;
     for(int x = 0; x < 32; x++)
     {
-    	GPIOA->ODR &= !(0x1f8); //clear bits 3-8 for RGB
+    	GPIOA->ODR &= ~(0x1f8); //clear bits 3-8 for RGB
     	GPIOA->ODR |= (1 & (imagedata[(imageline *3 * 32)+x])) << 3;
     	GPIOA->ODR |= (1 & (imagedata[(imageline *3 * 32)+x+1])) << 4;
     	GPIOA->ODR |= (1 & (imagedata[(imageline *3 * 32)+x + 2])) << 5;
@@ -81,20 +109,34 @@ void TIM6_DAC_IRQHandler()
     	GPIOA->ODR |= (1 & (imagedata[((imageline+8) *3 * 32)+x + 2])) << 8;
 
     	GPIOA->ODR |= 0x2; //Clock high
-    	GPIOA->ODR &= !(0x2); //Clock low
+    	GPIOA->ODR &= ~(0x2); //Clock low
     }
     GPIOA->ODR |= 0x4; //Latch high
-    GPIOA->ODR &= !(0x4); //Latch low
-    GPIOA->ODR &= !(1);
-    imageline = (imageline + 1) % 8;
+    GPIOA->ODR &= ~(0x4); //Latch low
+    GPIOA->ODR &= ~(1);
+    imageline +=1;
+    if(imageline > 7) imageline = 0;
+    TIM6->SR &= !(1);
 }
-
 
 int main(void)
 {
 	init_picture();
 	init_GPIO();
 	init_tim6();
-	while(1);
+
+	while(1)
+	{
+		init_picture();
+		for(int i = 0; i < 480; i++)
+		{
+			asm("nop");
+		}
+		picture_red();
+		for(int i = 0; i < 480; i++)
+		{
+			asm("nop");
+		}
+	}
 
 }
